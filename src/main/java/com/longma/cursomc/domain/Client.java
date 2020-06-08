79126f117eb976b799ec9467d5b1b2f53fc0af67
longma19900415 <longma19900415@outlook.com>
2020-06-08 19:58:47 +0800

    changed to jsonignore and section2 finished

--- src/main/java/com/longma/cursomc/domain/Client.java
+++ src/main/java/com/longma/cursomc/domain/Client.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.longma.cursomc.domain.enums.ClientType;
 
 @Entity
@@ -31,7 +30,6 @@ public class Client implements Serializable {
 	private String phyOrLegal;
 	private Integer type;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy="client")
 	private List<Address> addresses = new ArrayList<>();
 	
@@ -39,7 +37,7 @@ public class Client implements Serializable {
 	@CollectionTable(name="TELEPHONE")
 	private Set<String> telephones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy ="client")
 	private List<Order> orders = new ArrayList<>();
 	
