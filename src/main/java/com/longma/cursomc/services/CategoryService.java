cdf304d3da5f98b700311fa2198a20b225794193
longma19900415 <longma19900415@outlook.com>
2020-06-09 00:23:53 +0800

    delete with exception

--- src/main/java/com/longma/cursomc/services/CategoryService.java
+++ src/main/java/com/longma/cursomc/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.longma.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.longma.cursomc.domain.Category;
 import com.longma.cursomc.repositories.CategoryRepository;
+import com.longma.cursomc.services.exceptions.DataIntegrityException;
 import com.longma.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoryService {
 	public Category update(Category obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Impossible to delete category with products");
+		}
 	}
 }
